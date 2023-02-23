82fac894444b5e7732b5679dec584a2c509c8e16
Mokeitech <126171819+Mokeitech@users.noreply.github.com>
2023-02-23 21:43:27 +0100

    Add files via upload

---
+++ order.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Mint Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'HPF 4k Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    }
+]
